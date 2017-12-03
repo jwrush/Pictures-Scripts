8292c60e0a06e5d3aced15073dbd2cbed41d99e2
Rush <jwrush@gmail.com>
2017-12-03 13:09:45 -0600

    initial commit

---
+++ intake_pics.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+INDIR="/home/rush/Pictures/in"
+
+#takes one param, the file pattern to intake
+_intake () {
+  if [ "$PWD" != $INDIR ]; then
+    echo "Emptying $1 from $PWD to $INDIR ..."
+    mv $PWD/$1 $INDIR/
+  fi
+
+  echo "Moving $1 from Downloads to $PWD ..."
+
+  NUM_MOVED=$(mv ~/Downloads/$1 ./ | wc -l)
+
+  echo "... $NUM_MOVED moved."
+}
+
+_intake "*.jpg"
+_intake "*.png"
